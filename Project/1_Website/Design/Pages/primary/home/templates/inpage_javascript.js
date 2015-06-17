96168603aa400efa6adcd1ac78d1768d71ebecef
unknown <TURNER@.(none)>
2015-06-17 17:11:34 +0800

    Ashley Nohay homepage

--- Project/1_Website/Design/Pages/primary/home/templates/inpage_javascript.js
+++ Project/1_Website/Design/Pages/primary/home/templates/inpage_javascript.js
@@ -17,5 +17,31 @@ $(document).ready(function(){
 	  e.preventDefault()
 	  $(this).tab('show')
 	});
+
+	/* 
+	* Test Locations
+	* Austin lat/long: 30.2676,-97.74298
+	* Austin WOEID: 2357536
+	*/
+	$(document).ready(function() {
+	  loadWeather('Philippines',''); //@params location, woeid
+	});
+
+	function loadWeather(location, woeid) {
+	  $.simpleWeather({
+	    location: location,
+	    woeid: woeid,
+	    unit: 'c',
+	    success: function(weather) {
+	      html = '<h2></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+	     
+	      
+	      $("#weather").html(html);
+	    },
+	    error: function(error) {
+	      $("#weather").html('<p>'+error+'</p>');
+	    }
+	  });
+	}
 });
 
