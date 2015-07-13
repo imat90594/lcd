d995439c1aefbba7e01bc8d81a7a3f8eb4460fb9
unknown <TURNER@.(none)>
2015-07-13 18:44:25 +0800

    ashley nohay office commit

--- Project/1_Website/Design/Pages/primary/city_guide/templates/inpage_javascript.js
+++ Project/1_Website/Design/Pages/primary/city_guide/templates/inpage_javascript.js
@@ -3,6 +3,24 @@ $(document).ready(function(){
 	    theme:"rounded-dots",
 	});
 	
+	$('#linkScroll1').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $( $(this).attr('href') ).offset().top
+	    }, 500);
+
+	});
+	$('#linkScroll2').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $( $(this).attr('href') ).offset().top
+	    }, 500);
+
+	});
+	$('#linkScroll3').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $( $(this).attr('href') ).offset().top
+	    }, 500);
+
+	});
 });
 
 
